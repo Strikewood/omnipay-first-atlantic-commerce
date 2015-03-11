44f8bf7aae035d30f37485abecbb8d5f615c97e4
Einkoro <jpingel@bitpiston.com>
2015-03-11 09:50:41 -0700

    Methods common to authorize and modification responses moved to trait

--- src/Message/AuthorizeResponse.php
+++ src/Message/AuthorizeResponse.php
@@ -5,12 +5,15 @@ namespace Omnipay\FirstAtlanticCommerce\Message;
 use Omnipay\Common\Exception\InvalidResponseException;
 use Omnipay\Common\Message\RequestInterface;
 use Omnipay\FirstAtlanticCommerce\Message\AbstractResponse;
+use Omnipay\FirstAtlanticCommerce\Message\TransactionResponseTrait;
 
 /**
  * FACPG2 XML Authorize Response
  */
 class AuthorizeResponse extends AbstractResponse
 {
+    use TransactionResponseTrait;
+
     /**
      * Constructor
      *
@@ -53,35 +56,5 @@ class AuthorizeResponse extends AbstractResponse
                 throw new InvalidResponseException('Signature verification failed');
             }
         }
-    }
-
-    /**
-     * Return whether or not the response was successful
-     *
-     * @return boolean
-     */
-    public function isSuccessful()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ResponseCode']) and '1' === $this->data['CreditCardTransactionResults']['ResponseCode'];
-    }
-
-    /**
-     * Return the response message
-     *
-     * @return string
-     */
-    public function getMessage()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ReasonCodeDescription']) ? $this->data['CreditCardTransactionResults']['ReasonCodeDescription'] : null;
-    }
-
-    /**
-     * Return transaction reference
-     *
-     * @return string
-     */
-    public function getTransactionReference()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ReferenceNumber']) ? $this->data['CreditCardTransactionResults']['ReferenceNumber'] : null;
     }
 }
--- src/Message/TransactionModificationResponse.php
+++ src/Message/TransactionModificationResponse.php
@@ -5,12 +5,15 @@ namespace Omnipay\FirstAtlanticCommerce\Message;
 use Omnipay\Common\Exception\InvalidResponseException;
 use Omnipay\Common\Message\RequestInterface;
 use Omnipay\FirstAtlanticCommerce\Message\AbstractResponse;
+use Omnipay\FirstAtlanticCommerce\Message\TransactionResponseTrait;
 
 /**
  * FACPG2 Transaction Modification Response
  */
 class TransactionModificationResponse extends AbstractResponse
 {
+    use TransactionResponseTrait;
+
     /**
      * Constructor
      *
@@ -25,35 +28,5 @@ class TransactionModificationResponse extends AbstractResponse
 
         $this->request = $request;
         $this->data    = $this->xmlDeserialize($data);
-    }
-
-    /**
-     * Return whether or not the response was successful
-     *
-     * @return boolean
-     */
-    public function isSuccessful()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ResponseCode']) and '1' === $this->data['CreditCardTransactionResults']['ResponseCode'];
-    }
-
-    /**
-     * Return the response message
-     *
-     * @return string
-     */
-    public function getMessage()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ReasonCodeDescription']) ? $this->data['CreditCardTransactionResults']['ReasonCodeDescription'] : null;
-    }
-
-    /**
-     * Return transaction reference
-     *
-     * @return string
-     */
-    public function getTransactionReference()
-    {
-        return isset($this->data['CreditCardTransactionResults']['ReferenceNumber']) ? $this->data['CreditCardTransactionResults']['ReferenceNumber'] : null;
     }
 }
